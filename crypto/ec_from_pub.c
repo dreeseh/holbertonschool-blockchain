6309e3e79bee87992b66ce2d784ff5b1bf56db53
dreeseh <1859@holbertonschool.com>
2023-05-23 21:46:49 +0000

    print to stdout

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -8,9 +8,9 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	BIGNUM *bn_pub_x = BN_bin2bn(pub + 1, EC_PUB_LEN / 2, NULL);
 	BIGNUM *bn_pub_y = BN_bin2bn(pub + EC_PUB_LEN / 2 + 1, EC_PUB_LEN / 2, NULL);
 
-	if (!pub)
+	if (pub[EC_PUB_LEN != 65])
 	{
-		fprintf(stderr, "ec_from_pub() failed\n");
+		printf(stderr, "ec_from_pub() failed\n");
 		return (NULL);
 	}
 	if (key == NULL)
