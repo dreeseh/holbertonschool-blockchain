eca3efddba2980f7e2942c108e9b2581d826392a
dreeseh <1859@holbertonschool.com>
2023-05-23 21:48:14 +0000

    still had stderr in there

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -10,7 +10,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 
 	if (pub[EC_PUB_LEN != 65])
 	{
-		printf(stderr, "ec_from_pub() failed\n");
+		printf("ec_from_pub() failed\n");
 		return (NULL);
 	}
 	if (key == NULL)
