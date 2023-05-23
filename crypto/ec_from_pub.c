1f90811774c6b78cc9f2bb2cc87dd2f78dd1f2c4
dreeseh <1859@holbertonschool.com>
2023-05-23 21:44:17 +0000

    spelling err #2

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -8,7 +8,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	BIGNUM *bn_pub_x = BN_bin2bn(pub + 1, EC_PUB_LEN / 2, NULL);
 	BIGNUM *bn_pub_y = BN_bin2bn(pub + EC_PUB_LEN / 2 + 1, EC_PUB_LEN / 2, NULL);
 
-	if (pub == NULL)
+	if (!pub)
 	{
 		fprintf(stderr, "ec_from_pub() failed\n");
 		return (NULL);
