4482aa96852a841fbd34193737b5594a2ec755aa
dreeseh <1859@holbertonschool.com>
2023-05-23 21:42:38 +0000

    spelling err

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -10,7 +10,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 
 	if (pub == NULL)
 	{
-		fprintf(stderr, "ec_to_pub() failed\n");
+		fprintf(stderr, "ec_from_pub() failed\n");
 		return (NULL);
 	}
 	if (key == NULL)
