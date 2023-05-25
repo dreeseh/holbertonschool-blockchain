57a3c6b746d91e5ba69e82fac79b65a52fab6c60
dreeseh <1859@holbertonschool.com>
2023-05-25 19:14:58 +0000

    files for task 5

---
+++ crypto/ec_load.c
@@ -0,0 +1,49 @@
+#include "hblk_crypto.h"
+
+EC_KEY *ec_load(char const *folder)
+{
+	EC_KEY *private_key, *public_key;
+	FILE *file;
+	char key_path[1024];
+
+	if (!folder)
+		return (NULL);
+
+	/* load private key */
+	sprintf(key_path, "%s/%s", folder, PRI_FILENAME);
+	file = fopen(key_path, "r");
+	if (!file)
+		return (NULL);
+
+	private_key = PEM_read_ECPrivateKey(file, NULL, NULL, NULL);
+	if (!private_key)
+	{
+		fclose(file);
+		return (NULL);
+	}
+	fclose(file);
+
+	/* load public key */
+	sprintf(key_path, "%s/%s", folder, PUB_FILENAME);
+	file = fopen(key_path, "r");
+	if (!file)
+		return (NULL);
+
+	public_key = PEM_read_EC_PUBKEY(file, NULL, NULL, NULL);
+	if (!public_key)
+	{
+		fclose(file);
+		return (NULL);
+	}
+	fclose(file);
+
+	/* set public key */
+	if (EC_KEY_set_public_key(private_key, EC_KEY_get0_public_key(public_key)) == 0)
+	{
+		EC_KEY_free(private_key);
+		EC_KEY_free(public_key);
+		return (NULL);
+	}
+	EC_KEY_free(public_key);
+	return (private_key);
+}
