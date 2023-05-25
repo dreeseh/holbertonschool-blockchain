c4f602a02a9a0a3e5a6f34a1fa776e2cf36fe4fa
dreeseh <1859@holbertonschool.com>
2023-05-25 16:16:54 +0000

    betty fixes

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,5 +1,13 @@
 #include "hblk_crypto.h"
 
+/**
+ * ec_save - saves an existing EC key pair on the disk
+ *
+ * @key: points to the EC key pair to be saved on disk
+ * @folder: the path to the folder in which to save the keys
+ *
+ * Return: 1 or 0 upon success or failure
+ */
 int ec_save(EC_KEY *key, char const *folder)
 {
 	char path[256];
@@ -40,6 +48,6 @@ int ec_save(EC_KEY *key, char const *folder)
 		return (0);
 	}
 	fclose(file);
-	
+
 	return (1);
 }
