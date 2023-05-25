e946686f760b65b450f63ba3680d28e6bafc0a9c
dreeseh <1859@holbertonschool.com>
2023-05-25 19:17:35 +0000

    forgot to push my new header

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #include <sys/stat.h>
 
@@ -54,5 +55,6 @@ EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
+EC_KEY *ec_load(char const *folder);
 
 #endif /* _HBLK_CRYPTO_H */
