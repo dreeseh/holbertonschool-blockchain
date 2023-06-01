ab32ba746c674de4e345f2670dd403d7e075f08e
dreeseh <1859@holbertonschool.com>
2023-06-01 14:26:16 +0000

    revert to old file for ec_load.c

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -2,30 +2,12 @@
 
 EC_KEY *ec_load(char const *folder)
 {
-	EC_KEY *new_key;
+	EC_KEY *private_key, *public_key;
 	FILE *file;
 	char key_path[PATH_MAX];
-	struct stat status;
 
-	if (!folder || stat(folder, &status) == -1)
+	if (!folder)
 		return (NULL);
-
-	new_key = EC_KEY_new_by_curve_name(EC_CURVE);
-
-	/* load public key */
-	sprintf(key_path, "%s/%s", folder, PUB_FILENAME);
-	file = fopen(key_path, "r");
-	if (!file)
-		return (NULL);
-
-	PEM_read_EC_PUBKEY(file, &new_key, NULL, NULL);
-	if (new_key)
-	{
-		EC_KEY_free(new_key);
-		fclose(file);
-		return (NULL);
-	}
-	fclose(file);
 
 	/* load private key */
 	sprintf(key_path, "%s/%s", folder, PRI_FILENAME);
@@ -33,13 +15,28 @@ EC_KEY *ec_load(char const *folder)
 	if (!file)
 		return (NULL);
 
-	PEM_read_ECPrivateKey(file, &new_key, NULL, NULL);
-	if (!new_key)
+	private_key = PEM_read_ECPrivateKey(file, NULL, NULL, NULL);
+	if (!private_key)
 	{
-		EC_KEY_free(new_key);
 		fclose(file);
 		return (NULL);
 	}
 	fclose(file);
-	return (new_key);
+
+	/* load public key */
+	sprintf(key_path, "%s/%s", folder, PUB_FILENAME);
+	file = fopen(key_path, "r");
+	if (!file)
+		return (NULL);
+
+	public_key = PEM_read_EC_PUBKEY(file, NULL, NULL, NULL);
+	if (!public_key)
+	{
+		fclose(file);
+		return (NULL);
+	}
+	fclose(file);
+
+	EC_KEY_free(public_key);
+	return (private_key);
 }
