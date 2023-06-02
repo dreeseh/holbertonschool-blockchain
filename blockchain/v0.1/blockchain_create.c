d21bb169d3dba839d1395d2554aed116bb3b3599
dreeseh <1859@holbertonschool.com>
2023-06-02 17:35:46 +0000

    initial project structure & task 0 for initial checks

---
+++ blockchain/v0.1/blockchain_create.c
@@ -0,0 +1,34 @@
+#include "blockchain.h"
+
+/**
+ * blockchain_create - creates a Blockchain structure, and initializes it
+ *
+ * Return: pointer to the new blockchain or NULL upon failure
+ */
+blockchain_t *blockchain_create(void)
+{
+	blockchain_t *new_blockchain;
+	block_t *new_block;
+
+	new_blockchain = malloc(sizeof(blockchain_t));
+	if (!new_blockchain)
+		return (NULL);
+
+	new_block = malloc(sizeof(block_t));
+	if (!new_block)
+	{
+		free(new_blockchain);
+		return (NULL);
+	}
+
+	new_blockchain->chain = llist_create(MT_SUPPORT_FALSE);
+
+	new_block->info.timestamp = GENESIS_TIMESTAMP;
+	memcpy(&(new_block->data.buffer), GENESIS_DATA, GENESIS_DATA_LEN);
+	new_block->data.len = GENESIS_DATA_LEN;
+	memcpy(&(new_block->hash), GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
+	llist_add_node(new_blockchain->chain, new_block, ADD_NODE_FRONT);
+
+	return (new_blockchain);
+}
