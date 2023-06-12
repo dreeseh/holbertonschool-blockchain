950977a1634c0a32f3a5a1474d4716c99250c6b1
dreeseh <1859@holbertonschool.com>
2023-06-12 19:01:55 +0000

    linter fixes for block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -12,7 +12,7 @@ uint8_t *block_hash(block_t const *block,
 		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	sha256((int8_t const *)block,
-		sizeof(block_info_t) + block->data.len, 
+		sizeof(block_info_t) + block->data.len,
 		hash_buf);
 
 	return (hash_buf);
